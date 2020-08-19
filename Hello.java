dbe542b67409e0aa5b046b66e721e7d5014b575b
knightea2 <69914816+knightea2@users.noreply.github.com>
2020-08-19 12:12:59 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*************************************************************Hello.java
+**************************************************************Evan Knight
+*8/19/20
+*CMSC 255 Sec. 003
+************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Evan Knight>");
+		System.out.println("<Lab 1>");
+		System.out.println("<First Lab>");
+		System.out.println("<8/19/20>");
+		System.out.println("<CMSC 255 Sec. 003>");
+		System.out.println();
+	}
+}
